84a3c90896cd358bf054fbeac085d19856537730
RobertVDLeeuw <robert.van.der.leeuw@gmail.com>
2025-02-24 14:14:19 +0100

    Cleaned BFS, had small issue with bandaid.

--- scraper/content_processor.py
+++ scraper/content_processor.py
@@ -6,6 +6,7 @@ from urllib.parse import urljoin
 
 from time import sleep
 
+
 class ParsingError(Exception):
     """
     Custom exception raised when content parsing fails.
@@ -23,40 +24,37 @@ def bfs_site(starting_url: str, auth_info=None, slowdown_s: float = 0.05) -> dic
 
     while links:
         link = links.pop()
-        
-        if link in pages:
-            continue  # Filtering bandaid.
-
         print("Processing", link)
 
         html = get_content(link, auth_info)
-        if not html:
-            continue  # Skipping failed pages (find better way to handle this).
-
         pages[link] = html_to_md(html)
 
         # Add new links which aren't already processed.
-        links |= get_all_links(html, base_url) ^ set(pages.keys())
+        links |= get_all_links(html, base_url) - set(pages)
 
         sleep(slowdown_s)  # So we don't accidentaly DoS the docs.
     return pages
 
-def get_content(url: str, auth_info=None) -> str:
+
+def get_content(url: str, auth_info=None) -> str | None:
     try:
         r = get(url)
 
         if not r.status_code == 200:
             print(f"Couldn't access page: {url} (HTTP {r.status_code})")  
-            
             return ""
 
         return r.content
     except RequestException as e:
-        print(f"Error fetching content from {self.link}: {e}")
+        print(f"Couldn't access page: {url}, {e}")
+
+        return ""
         # raise ParsingError
+
 
 def html_to_md(html: str) -> str:
     return md(html)
+
 
 def get_all_links(html: str, base_url: str, internal_only=True) -> set[str]:
     soup = BeautifulSoup(html, 'html.parser')
@@ -65,4 +63,8 @@ def get_all_links(html: str, base_url: str, internal_only=True) -> set[str]:
     links = {urljoin(base_url, a.get('href')).split('#')[0] for a in soup.find_all('a')}
 
     return {l for l in links if l.startswith(base_url)} if internal_only else links
+
+
+if __name__ == "__main__":
+    print(bfs_site("https://developer-specs.company-information.service.gov.uk/companies-house-identity-service/reference").keys())
 
