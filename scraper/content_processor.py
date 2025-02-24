6b89168d8e53352b12a3d1042bc901223e6f5fef
33may <eternalmay33@gmail.com>
2025-02-24 11:16:23 +0100

    start with parsing env

---
+++ scraper/content_processor.py
@@ -0,0 +1,10 @@
+class ContentProcessor:
+    def __init__(self, link, protected = False):
+        self.link = link
+        self.protected = protected
+
+    def get_content(self):
+
+    def html_to_md(self):
+
+    def get_all_links(self):
