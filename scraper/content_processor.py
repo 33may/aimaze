783162d869913e6d429aa3ad80884014e9850c7a
Robert <robert@rrcfvdl-deb.home>
2025-02-24 11:47:25 +0100

    Base scraper done, added gitignore.

--- scraper/content_processor.py
+++ scraper/content_processor.py
@@ -1,10 +1,24 @@
-class ContentProcessor:
-    def __init__(self, link, protected = False):
-        self.link = link
-        self.protected = protected
+from requests import get
+from bs4 import BeautifulSoup
+from markdownify import markdownify as md
+from urllib.parse import urljoin
 
-    def get_content(self):
 
-    def html_to_md(self):
+def get_content(url: str) -> str:
+    r = get(url)
 
-    def get_all_links(self):
+    if not r.status_code == 200:
+        raise Exception(f"Couldn't access page: {url} (HTTP {r.status_code})")  # TODO: Better way to handle this than nuke button.
+
+    return r.content
+
+
+def html_to_md(html: str) -> str:
+    return md(html)    
+    
+
+def get_all_links(html: str, base_url: str) -> list[str]:
+    soup = BeautifulSoup(html, 'html.parser')
+
+    links = [a.get('href') for a in soup.find_all('a')]
+    return [urljoin(base_url, l) for l in links]
